b9b3dacb3cd735861a477ac3d9db5e58f44ec5ba
Ahmed <73728149+AhmedMudkip@users.noreply.github.com>
2024-07-16 00:01:54 +0400

    refactor: render functions and getPokemon function

--- script.js
+++ script.js
@@ -1,43 +1,15 @@
-async function getPokemon(identifier) {
-    identifier = identifier.toLowerCase();
-
-    try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${identifier}`);
-        const jsonData = await response.json();
-
-        const name = jsonData["name"].charAt(0).toUpperCase() + jsonData["name"].slice(1);
-
-        // currently the description is getting the first description found based on the first version present through the api
-        // if a pokemon is present in version 1 and version 3, the description will always be for version 1
-        const description = jsonData["flavor_text_entries"][2]["flavor_text"].split("\n").join(" ");
-
-        const id = await getID(identifier);
-        console.log(`${id}: ${name}\n${description}`);
-    } catch (error) {
-        console.error(`Receieved error: ${error}`);
-    }
-}
-
-async function getID(identifier) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${identifier}`);
+async function loadPokemon() {
+    const input = document.getElementById("pokemon");
+    const pokemonID = input.value.toLowerCase();
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`);
     const jsonData = await response.json();
 
-    return jsonData["id"];
-}
-
-function loadPokemon() {
     async function loadImage() {
         try {
-            const input = document.getElementById("pokemon");
-            const pokemonID = input.value.toLowerCase();
-
-            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`);
-            const jsonData = await response.json();
-
             const pokemonImage = jsonData["sprites"]["front_default"];
 
             // Debugging
-            console.log(`Image link for: ${pokemonImage}`);
+            console.log(`Image link: ${pokemonImage}`);
 
             const image = document.createElement("img");
             image.src = pokemonImage;
@@ -51,13 +23,10 @@ function loadPokemon() {
 
     async function loadAudio() {
         try {
-            const input = document.getElementById("pokemon");
-            const pokemonID = input.value.toLowerCase();
-
-            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`);
-            const jsonData = await response.json();
-
             const pokemonCry = jsonData["cries"]["latest"];
+
+            // Debugging
+            console.log(`Audio link: ${pokemonCry}`);
 
             const audio = document.createElement("audio");
             audio.src = pokemonCry;
@@ -70,6 +39,34 @@ function loadPokemon() {
         }
     }
 
+    async function getName(pokemonID) {
+        async function getID(pokemonID) {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${identifier}`);
+            const jsonData = await response.json();
+
+            return jsonData["id"];
+        }
+
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${identifier}`);
+            const jsonData = await response.json();
+
+            const name = jsonData["name"].charAt(0).toUpperCase() + jsonData["name"].slice(1);
+
+            // currently the description is getting the first description found based on the first version present through the api
+            // if a pokemon is present in version 1 and version 3, the description will always be for version 1
+            const description = jsonData["flavor_text_entries"][2]["flavor_text"]
+                .split("\n")
+                .join(" ");
+
+            const id = await getID(identifier);
+            console.log(`${id}: ${name}\n${description}`);
+        } catch (error) {
+            console.error(`Receieved error: ${error}`);
+        }
+    }
+
     loadImage();
     loadAudio();
+    // getName(pokemonID);
 }
