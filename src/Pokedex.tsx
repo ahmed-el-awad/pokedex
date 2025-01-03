681637b71a73ffa49db434a0f977baf282694a12
Ahmed <73728149+ahmed-el-awad@users.noreply.github.com>
2025-01-03 22:22:04 +0400

    refactor: convert dom manipulation to react hooks

--- src/Pokedex.tsx
+++ src/Pokedex.tsx
@@ -1,101 +1,120 @@
-async function loadPokemon() {
-  const input = document.getElementById("pokemon");
-  const inputField = document.getElementById("inputField");
-  const pokemonID = input.value.toLowerCase(); // name or number
+import { useState } from "react";
 
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`);
-  const jsonData = await response.json();
+function Pokedex() {
+  const [pokemonID, setPokemonID] = useState("");
+  const [pokemonImage, setPokemonImage] = useState("");
+  const [isImagePresent, setImagePresent] = useState(false);
+  const [pokemonDescription, setPokemonDescription] = useState("");
+  const [pokemonName, setPokemonName] = useState("");
+  // TODO: fix description language being other countries, currently hard-coded to english
+  const language = "en";
 
-  async function loadImage() {
+  // TODO: account for names that have hyphens or periods (ex: Mr. Mime, Ho-Oh),
+  //       maybe hardcode them since there aren't many
+  // TODO: account for mega evolutions by loading the numbers, names, and descriptions from their original forms
+  // TODO: account for different forms (the logic will be the same as megas)
+  async function loadPokemon(pokemonID: string | number) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`);
+    const jsonData = await response.json();
+    console.log(jsonData);
+
+    // takes a pokemonID only, not jsonData since it uses a difference api endpoint
+    await loadName(pokemonID);
+    await loadImage(jsonData);
+    await loadAudio(jsonData);
+  }
+
+  async function loadName(pokemonID: string | number) {
+    try {
+      // different api endpoint, this one contains the description
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonID}`);
+      const jsonData = await response.json();
+      const name = jsonData["name"].charAt(0).toUpperCase() + jsonData["name"].slice(1);
+      const number = jsonData["id"];
+
+      // currently the description is getting the first description found based on the first version present through the api
+      // if a pokemon is present in version 1 and version 3, the description will always be for version 1
+
+      // TODO: gets the description of a pokemon based of the language set by the user
+      // TODO: some pokemons do not have a description in certain languages
+      //       make it error and show that to the user (maybe display the desc in english?)
+      let desc = jsonData["flavor_text_entries"].find((obj) => {
+        if (obj["language"]["name"] === language) {
+          return obj;
+        }
+      });
+
+      // remove that annoying arrow
+      desc = desc.flavor_text.replace("\f", " ");
+
+      // Debugging
+      console.log(`${number}: ${name}\n${desc}`);
+
+      setPokemonName(`${number + ": " + name}`);
+      setPokemonDescription(desc);
+    } catch (error) {
+      console.error(`Receieved error: ${error}`);
+    }
+  }
+
+  async function loadImage(jsonData) {
     try {
       const pokemonImage = jsonData["sprites"]["front_default"];
 
       // Debugging
       console.log(`Image link: ${pokemonImage}`);
 
-      const image = document.createElement("img");
-      image.src = pokemonImage;
-
-      inputField.insertAdjacentElement("afterend", image);
+      setPokemonImage(pokemonImage);
+      setImagePresent(true);
     } catch (error) {
-      console.log("Failed to load image.");
       console.error(error);
     }
   }
 
-  async function loadAudio() {
+  async function loadAudio(jsonData) {
     try {
       const pokemonCry = jsonData["cries"]["latest"];
 
       // Debugging
       console.log(`Audio link: ${pokemonCry}`);
 
-      const audio = document.createElement("audio");
-      audio.src = pokemonCry;
+      const audio = new Audio(pokemonCry);
       audio.play();
-
-      inputField.insertAdjacentElement("afterend", audio);
     } catch (error) {
       console.error(error);
-      console.log("Failed to load audio.");
     }
   }
 
-  // TODO: account for names that have hyphens or periods (ex: Mr. Mime, Ho-Oh)
-  // TODO: account for mega evolutions by loading the numbers, names, and descriptions from their original forms
-  // TODO: account for different forms (the logic will be the same as megas)
-  // TODO: fix description language being other countries                                                                                                                               dSCDZVX VXD
-  async function loadName(pokemonID) {
-    try {
-      // different api endpoint, this one contains the description
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonID}`);
-      const jsonData = await response.json();
-      const name = jsonData["name"].charAt(0).toUpperCase() + jsonData["name"].slice(1);
-      const id = jsonData["id"];
-
-      // currently the description is getting the first description found based on the first version present through the api
-      // if a pokemon is present in version 1 and version 3, the description will always be for version 1
-      const desc = jsonData["flavor_text_entries"][2]["flavor_text"].split("\n").join(" ");
-
-      // Debugging
-      console.log(`${id}: ${name}\n${desc}`);
-
-      const header = document.createElement("h1");
-      header.innerText = `${id}: ${name}`;
-
-      const description = document.createElement("p");
-      description.innerText = desc;
-
-      inputField.insertAdjacentElement("afterend", description);
-      inputField.insertAdjacentElement("afterend", header);
-    } catch (error) {
-      console.error(`Receieved error: ${error}`);
-    }
-  }
-
-  loadName(pokemonID);
-  loadImage();
-  loadAudio();
-}
-function pressedEnter() {
-  if (event.key === "Enter") {
-    loadPokemon();
-  }
-}
-
-function Pokedex() {
   // if a user pressed enter after writing a pokemon's id or name
+  function pressedEnter(pokemonID: string | number) {
+    if (event.key === "Enter") {
+      loadPokemon(pokemonID);
+    }
+  }
 
   return (
-    <div id="inputField">
+    <div>
       <input
-        id="pokemon"
         style={{ width: "15rem", marginBottom: "0.25rem", marginRight: "0.25rem" }}
         placeholder="Enter a Pokemon's name or number"
         type="text"
-        onKeyDown={pressedEnter}
+        value={pokemonID}
+        onChange={(e) => setPokemonID(e.target.value)}
+        onKeyDown={() => pressedEnter(pokemonID)}
       />
-      <button onClick={loadPokemon}>Submit</button>
+      <button
+        onClick={async () => {
+          loadPokemon(pokemonID);
+          const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`);
+          const jsonData = await response.json();
+          loadImage(jsonData);
+        }}
+      >
+        Submit
+      </button>
+      <h1>{pokemonName}</h1>
+      <p>{pokemonDescription}</p>
+      {isImagePresent && <img style={{ display: "block" }} src={pokemonImage} alt="pokemon" />}
     </div>
   );
 }
