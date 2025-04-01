ab5d4bcdbcabd9ec10a28edc08904f5e61aa90d3
Ahmed <73728149+ahmed-el-awad@users.noreply.github.com>
2025-04-01 20:49:04 +0400

    fix: pokemon data being referenced
    now it correctly renders when other pokemons are searched for

--- src/Pokedex.tsx
+++ src/Pokedex.tsx
@@ -2,46 +2,39 @@ import { useState } from "react";
 import PokemonData from "./PokemonData";
 import { Pokemon } from "./interface";
 
-const pokemons: Pokemon[] = [];
-
 function Pokedex() {
   const [pokemonID, setPokemonID] = useState("");
-  const [pokemonImage, setPokemonImage] = useState("");
-  const [isImagePresent, setImagePresent] = useState(false);
-  const [pokemonDescription, setPokemonDescription] = useState("");
-  const [pokemonName, setPokemonName] = useState("");
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
   // TODO: fix description language being other countries, currently hard-coded to english
-  const language = "en";
+  const language: string = "en";
 
   // TODO: account for names that have hyphens or periods (ex: Mr. Mime, Ho-Oh),
   //       maybe hardcode them since there aren't many
   // TODO: account for mega evolutions by loading the numbers, names, and descriptions from their original forms
   // TODO: account for different forms (the logic will be the same as megas)
+
+  // in terms of numbers, this can be hardcoded to have a range 1-1025
+  // instead of sending the request to the api and causing load
   async function loadPokemon(pokemonID: string | number) {
-    const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${pokemonID}`,
-    );
-    const jsonData: Response = await response
-      .json()
-      .catch(() => window.alert("pokemon doesn't exist"));
-    console.log(jsonData);
-
-    // takes a pokemonID only, not jsonData since it uses a difference api endpoint
-    await loadName(pokemonID);
-    await loadImage(jsonData);
-    await loadAudio(jsonData);
-  }
-
-  async function loadName(pokemonID: string | number) {
     try {
-      // different api endpoint, this one contains the description
+      // used to obtain image and audio
       const response = await fetch(
-        `https://pokeapi.co/api/v2/pokemon-species/${pokemonID}`,
+        `https://pokeapi.co/api/v2/pokemon/${pokemonID}`,
       );
       const jsonData = await response.json();
+      console.log(jsonData);
+
+      // used to obtain name, number, and description
+      const speciesResponse = await fetch(
+        `https://pokeapi.co/api/v2/pokemon-species/${pokemonID}`,
+      );
+      const speciesData = await speciesResponse.json();
+
       const name =
-        jsonData["name"].charAt(0).toUpperCase() + jsonData["name"].slice(1);
-      const number = jsonData["id"];
+        speciesData["name"].charAt(0).toUpperCase() +
+        speciesData["name"].slice(1);
+      const number = speciesData["id"];
 
       // currently the description is getting the first description found based on the first version present through the api
       // if a pokemon is present in version 1 and version 3, the description will always be for version 1
@@ -49,7 +42,7 @@ function Pokedex() {
       // TODO: gets the description of a pokemon based of the language set by the user
       // TODO: some pokemons do not have a description in certain languages
       //       make it error and show that to the user (maybe display the desc in english?)
-      let desc = jsonData["flavor_text_entries"].find((obj) => {
+      let desc = speciesData["flavor_text_entries"].find((obj) => {
         if (obj["language"]["name"] === language) {
           return obj;
         }
@@ -58,62 +51,45 @@ function Pokedex() {
       // remove that annoying arrow
       desc = desc.flavor_text.replace("\f", " ");
 
-      // Debugging
-      console.log(`${number}: ${name}\n${desc}`);
-
-      setPokemonName(`${number + ": " + name}`);
-      setPokemonDescription(desc);
-    } catch (error) {
-      console.error(`Receieved error: ${error}`);
-    }
-  }
-
-  async function loadImage(jsonData: any) {
-    try {
       const pokemonImage = jsonData["sprites"]["front_default"];
-
-      // Debugging
       console.log(`Image link: ${pokemonImage}`);
 
-      setPokemonImage(pokemonImage);
-      setImagePresent(true);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  async function loadAudio(jsonData) {
-    try {
       const pokemonCry = jsonData["cries"]["latest"];
-
-      // Debugging
       console.log(`Audio link: ${pokemonCry}`);
 
       const audio = new Audio(pokemonCry);
       audio.volume = 0.05;
       audio.play();
+
+      const newPokemon: Pokemon = {
+        pokemonName: number + ": " + name,
+        pokemonImage: pokemonImage,
+        pokemonDescription: desc,
+      };
+
+      return newPokemon;
     } catch (error) {
       console.error(error);
+      window.alert("pokemon doesn't exist");
+
+      return null;
     }
   }
 
   // if a user pressed enter after writing a pokemon's id or name
-  function pressedEnter(pokemonID: string | number) {
+  function pressedEnter(
+    event: React.KeyboardEvent,
+    pokemonID: string | number,
+  ) {
     if (event.key === "Enter") {
       loadPokemon(pokemonID);
     }
   }
 
-  function addToArray() {
-    pokemons.push({
-      isImagePresent,
-      pokemonDescription,
-      pokemonName,
-      pokemonImage,
-    });
-
-    console.log(pokemons);
+  async function addToArray(newPokemon: Pokemon) {
+    setPokemons((previousPokemons) => [newPokemon, ...previousPokemons]);
   }
+
   return (
     <>
       <input
@@ -122,32 +98,28 @@ function Pokedex() {
         type="text"
         value={pokemonID}
         onChange={(e) => setPokemonID(e.target.value)}
-        onKeyDown={() => pressedEnter(pokemonID)}
+        onKeyDown={(event) => pressedEnter(event, pokemonID)}
       />
       <button
         className="rounded-md border-2 border-gray-500 p-1"
         onClick={async () => {
-          loadPokemon(pokemonID);
+          const pokemon = await loadPokemon(pokemonID);
 
-          const response = await fetch(
-            `https://pokeapi.co/api/v2/pokemon/${pokemonID}`,
-          );
-          const jsonData = await response.json();
-          loadImage(jsonData);
-          addToArray();
+          if (pokemon) {
+            addToArray(pokemon);
+          }
         }}
       >
         Submit
       </button>
       {pokemons.length > 0 &&
-        pokemons.map((_, id) => {
+        pokemons.map((pokemon, id) => {
           return (
             <PokemonData
               key={id}
-              isImagePresent={isImagePresent}
-              pokemonImage={pokemonImage}
-              pokemonName={pokemonName}
-              pokemonDescription={pokemonDescription}
+              pokemonImage={pokemon.pokemonImage}
+              pokemonName={pokemon.pokemonName}
+              pokemonDescription={pokemon.pokemonDescription}
             />
           );
         })}
